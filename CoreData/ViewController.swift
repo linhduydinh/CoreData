fd20d0f33660493b3c3693e6a96111d76b01d1aa
Đinh Duy Linh <linhduydinh@gmail.com>
2017-12-21 22:50:58 +0700

    First check-in

---
+++ CoreData/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoreData
+//
+//  Created by MAC on 12/21/17.
+//  Copyright © 2017 MAC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
